0e0fd7bc55e45e0cef6c1eb387a41eca0609e1f3
EUSUNGKIM <jimmy0818@naver.com>
2024-07-18 12:07:52 +0900

    Hello update
    업데이트

--- MyGitignore/Program.cs
+++ MyGitignore/Program.cs
@@ -11,6 +11,8 @@ namespace MyGitignore
         static void Main(string[] args)
         {
             Console.WriteLine("Hello");
+            Console.WriteLine("Hello");
+            Console.WriteLine("Hello");
         }
     }
 }
